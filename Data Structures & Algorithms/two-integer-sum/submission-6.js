1d59a00b4259603089c68a61ab805c8c05ebca7b
Bailey J. Mora <158508797+BJ-Mora@users.noreply.github.com>
2026-04-16 22:30:03 -0700

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        // nums == array of int
+        // target == int 
+        // return i & j where nums[i] + nums[j] == target && i != j
+        // can assme that every input will have one pair of indeces that satisfy the cond
+        // return answer w smaller index first
+
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return [i, j]
+                }
+            }
+        }
+    }
+}
